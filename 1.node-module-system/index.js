a567e3376f36054917d8fa0aab0e0756b619dd85
unknown <muhamadrizkihasan@smkwikrama.sch.id>
2024-01-11 19:13:20 +0700

    Menambahkan folder module system

---
+++ 1.node-module-system/index.js
@@ -0,0 +1,10 @@
+// const fs = require('fs'); // core module
+// const cetakNama = require('./coba'); // local module
+// const moment  = require('moment'); // third party module / npm module / node_modules
+
+// const cetakNama = require('./coba');
+// const PI = require('./coba');
+
+const coba = require('./coba');
+
+console.log(coba.cetakNama('Hasan'), coba.PI, coba.mahasiswa.cetakMhs(), new coba.Orang);
